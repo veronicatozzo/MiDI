b7fa82853982997ce7c2ba21306643a458d227ff
Unknown <veronica.tozzo@gmail.com>
2018-07-19 11:17:02 +0200

    Added previous files, benchmark for single dataset multiple models

---
+++ midi/intermediate/ensemble/classification.py
@@ -0,0 +1,151 @@
+# Authors: Veronica Tozzo (veronica.tozzo@dibris.unige.it)
+#
+# License: BSD 3 clause
+
+from __future__ import division
+
+import warnings
+import numpy as np
+
+from ..base import ClassifierMixin
+from ..externals.joblib import Parallel, delayed
+
+
+from sklearn.utils.multiclass import check_classification_targets
+from sklearn.utils.validation import check_is_fitted
+
+__all__ = []
+
+
+class EnsembleClassifier(ClassifierMixin):
+    """Base class for Ensemble classifier.
+    """
+
+    def __init__(self, estimators='RLS', n_estimators=10,
+                 estimator_params=tuple(), n_jobs=1, random_state=None,
+                 verbose=0, class_weight=None):
+        self.estimators = estimators
+        self.n_estimators = n_estimators
+        self.estimator_params = estimator_params
+        self.n_jbos = n_jobs
+        self.random_state = random_state
+        self.verbose = verbose
+        self.class_weight = class_weight
+
+    def _validate_y_class_weight(self, y):
+        check_classification_targets(y)
+
+        y = np.copy(y)
+
+        for k in range(self.n_outputs_):
+            classes_k, _ = np.unique(y[:, k], return_inverse=True)
+            self.n_classes_.append(classes_k.shape[0])
+
+        if self.n_classes != 2:
+            raise ValueError("The estimator works only on 2-classes scenarios")
+        return y
+
+    def fit(self, X, y, )
+    def predict(self, X):
+        """Predict class for X.
+        The predicted class of an input sample is a vote by the trees in
+        the forest, weighted by their probability estimates. That is,
+        the predicted class is the one with highest mean probability
+        estimate across the trees.
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+        Returns
+        -------
+        y : array of shape = [n_samples] or [n_samples, n_outputs]
+            The predicted classes.
+        """
+        proba = self.predict_proba(X)
+
+        if self.n_outputs_ == 1:
+            return self.classes_.take(np.argmax(proba, axis=1), axis=0)
+
+        else:
+            n_samples = proba[0].shape[0]
+            predictions = np.zeros((n_samples, self.n_outputs_))
+
+            for k in range(self.n_outputs_):
+                predictions[:, k] = self.classes_[k].take(np.argmax(proba[k],
+                                                                    axis=1),
+                                                          axis=0)
+
+            return predictions
+
+    def predict_proba(self, X):
+        """Predict class probabilities for X.
+        The predicted class probabilities of an input sample are computed as
+        the mean predicted class probabilities of the trees in the forest. The
+        class probability of a single tree is the fraction of samples of the same
+        class in a leaf.
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+        Returns
+        -------
+        p : array of shape = [n_samples, n_classes], or a list of n_outputs
+            such arrays if n_outputs > 1.
+            The class probabilities of the input samples. The order of the
+            classes corresponds to that in the attribute `classes_`.
+        """
+        check_is_fitted(self, 'estimators_')
+        # Check data
+        X = self._validate_X_predict(X)
+
+        # Assign chunk of trees to jobs
+        n_jobs, _, _ = _partition_estimators(self.n_estimators, self.n_jobs)
+
+        # avoid storing the output of every estimator by summing them here
+        all_proba = [np.zeros((X.shape[0], j), dtype=np.float64)
+                     for j in np.atleast_1d(self.n_classes_)]
+        lock = threading.Lock()
+        Parallel(n_jobs=n_jobs, verbose=self.verbose, backend="threading")(
+            delayed(accumulate_prediction)(e.predict_proba, X, all_proba, lock)
+            for e in self.estimators_)
+
+        for proba in all_proba:
+            proba /= len(self.estimators_)
+
+        if len(all_proba) == 1:
+            return all_proba[0]
+        else:
+            return all_proba
+
+    def predict_log_proba(self, X):
+        """Predict class log-probabilities for X.
+        The predicted class log-probabilities of an input sample is computed as
+        the log of the mean predicted class probabilities of the trees in the
+        forest.
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+        Returns
+        -------
+        p : array of shape = [n_samples, n_classes], or a list of n_outputs
+            such arrays if n_outputs > 1.
+            The class probabilities of the input samples. The order of the
+            classes corresponds to that in the attribute `classes_`.
+        """
+        proba = self.predict_proba(X)
+
+        if self.n_outputs_ == 1:
+            return np.log(proba)
+
+        else:
+            for k in range(self.n_outputs_):
+                proba[k] = np.log(proba[k])
+
+            return proba
---
+++ midi/utils.py
@@ -0,0 +1,160 @@
+from __future__ import division
+
+import warnings
+import sys
+
+import numpy as np
+import pandas as pd
+
+from sklearn.utils import check_array, _check_consistency_with_train
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV, balanced_accuracy_score
+
+param_name = {'ridge': 'alpha',
+              'svm': 'C',
+              'logisticregression': 'C',
+              'randomforest': 'n_estimators'}
+
+
+def _get_parameters(model, params=np.logspace(-10, 10, 50)):
+    model = str(model).lower()
+    par = param_name.get(model)
+    if par is None:
+        warnings.warn("The method "+model+" is not allowed. Please choose "
+                      "between Ridge, SVM, LogisticRegression, RandomForest")
+        return None
+    if model == 'randomforest' and params.dtype is not 'int64':
+        warnings.warn("The parameters of a RandomForestClassifier must be "
+                      "integers representing the number of estimators used."
+                      "Casting the vector to int.")
+        params = params.astype(int)
+    return {par: params}
+
+
+def _get_model(model):
+    matching = {'ridge': RidgeClassifier(),
+                'svm': LinearSVC(),
+                'logisticregression': LogisticRegression(),
+                'randomforest': RandomForestClassifier()}
+    model = str(model).lower()
+    obj = matching.get(model)
+    if obj is None:
+        warnings.warn("The method "+model+" is not allowed. Please choose "
+                      "between Ridge, SVM, LogisticRegression, RandomForest")
+        return None
+    return obj
+
+
+def benchmark_with_multiple_models(
+        X, y,
+        models=['Ridge', 'SVM', 'Logisticregression', 'RandomForest'],
+        params_range=np.logspace(-10, 10, 50),
+        n_of_repetitions=100, n_split_for_cv=3,
+        verbose=0,
+        filename='results.pkl'):
+    """
+    Function that given a dataset test it with multiple models to check its
+    performance in classifying y.
+
+    Params
+    ------
+    X: array-like, shape(n_samples, n_features)
+        The input matrix.
+
+    y: array-like, shape(n_samples,)
+        The output matrix.
+
+    models: List of models the user wants to test.
+        Available options are: Ridge, SVM, LogisticRegression, RandomForest
+        Default: all
+
+    params_range: List of parameters range to use for the models.
+        Default: np.logspace(-10, 10, 50), it will be used indicriminatively
+        for all the models.
+
+    n_of_repetitions: int, default: 100
+        Number of times in which to split the input dataset to test the models.
+
+    n_split_for_cv: int, default: 3
+        Number of K fold in which to split the training to tune the parameters.
+
+    verbose: boolean, default:0
+        If it set to 1 the procedure will print outputs during the execution.
+
+    filename: string, default: results.csv
+        The file in which to save the final results.
+    """
+
+    X = check_array(X)
+    _check_consistency_with_train(X, y)
+
+    if len(params_range) == 1:
+        params_range = [params_range]*len(models)
+
+    if len(params_range) != len(models):
+        warnings.warn("The length of the parameters range list must be equal "
+                      "to the number of models. Found %d parameters "
+                      "specification and"
+                      " %d models" % (len(params_range), len(models)))
+        sys.exit(0)
+
+    columns = ['mean_score_tr', 'std_score_tr', 'mean_score_ts',
+               'std_score_ts', 'parameters', 'scores_tr', 'scores_ts',
+               'scores_CV', 'estimators', 'train_idx', 'test_idx']
+
+    df_results = pd.DataFrame(columns=columns)
+
+    for m, p in zip(models, params_range):
+        model = _get_model(m)
+        params = _get_parameters(m)
+
+        if model is None or params is None:
+            continue
+
+        tr_scores = []
+        estimators = []
+        trains_idx = []
+        test_idx = []
+        ts_scores = []
+        parameters = []
+        val_scores = []
+        sss = StratifiedShuffleSplit(n_splits=n_of_repetitions)
+
+        for train, test in sss.split(X, y):
+            x_tr = X[train, :]
+            y_tr = y[train]
+            x_ts = X[test, :]
+            y_ts = y[test]
+            model = RandomForestClassifier()
+            gscv = GridSearchCV(model, params, cv=n_split_for_cv)
+            gscv.fit(x_tr, y_tr)
+            model = gscv.best_estimator_
+            parameters.append(gscv.best_params_[param_name[m.lower()]])
+            val_scores.append(gscv.cv_results_)
+            tr_scores.append(balanced_accuracy_score(y_tr,
+                                                     model.predict(x_tr)))
+            ts_scores.append(balanced_accuracy_score(y_ts,
+                                                     model.predict(x_ts)))
+            trains_idx.append(train)
+            test_idx.append(test)
+            estimators.append(model)
+
+        means_tr = np.mean(tr_scores)
+        std_tr = np.std(tr_scores)
+        means_ts = np.mean(ts_scores)
+        std_ts = np.std(ts_scores)
+
+        if verbose:
+            print("Finished with model %s with scores in train %f+/-%f and "
+                  "in test %f+/-%f." % (m, means_tr, std_tr, means_ts, std_ts))
+        res = pd.DataFrame([[means_tr, std_tr, means_ts, std_ts, parameters,
+                             tr_scores, ts_scores, val_scores, estimators,
+                             trains_idx, test_idx]],
+                           columns=columns, index=[m])
+        df_results = df_results.append(res)
+
+    df_results.to_pickle(filename)
+    print(df_results[:, 0:5])
